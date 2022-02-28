d91df9d50609859944bb640e421c327dd0aca2b7
Ariel <ariel.fuentesgarcia@gmail.com>
2022-02-28 16:54:07 -0600

    other commit

--- src/Word.js
+++ src/Word.js
@@ -1,11 +1,11 @@
 import React, {useState} from 'react';
-import words from './words.json'
+import quotes from './quotes.json'
 
-const getRandom = () => Math.floor(Math.random() * words.length);
+const getRandom = () => Math.floor(Math.random() * quotes.length);
 
 const Word = () => {
 
-    const [word, setWord] = useState(words[getRandom()]);
+    const [word, setWord] = useState(quotes[getRandom()]);
     const colors = [
        "#FF5733",
        "#FFA833",
@@ -16,15 +16,15 @@ const Word = () => {
     ];
     const changePhrase = () =>{
         const random = getRandom();
-        setWord(words[random]);
+        setWord(quotes[random]);
     };
     const color = colors[Math.floor(Math.random() * 6)]
     document.body.style = `background: ${color}`;
 
     return (
         <div style={{color: color}}>
-            <h3>¡{word.cita}!</h3>
-            <p>-{word.autor}-</p>
+            <h3>¡{word.quote}!</h3>
+            <p>-{word.author}-</p>
           
             <button onClick={changePhrase}><i class="fa-solid fa-arrow-right-long"></i></button>
         <footer>  Made by Ariel F. G.</footer>
